3e2e119dcc9e7474e987ed630359e5e5374991a8
Joseph Seamons <josephseamons@hotmail.com>
2018-09-05 05:52:54 -0700

    Initial Commit

---
+++ GitHubExampleXC9/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  GitHubExampleXC9
+//
+//  Created by Joseph Seamons on 9/5/18.
+//  Copyright © 2018 Joseph Seamons. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ GitHubExampleXC9/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GitHubExampleXC9
+//
+//  Created by Joseph Seamons on 9/5/18.
+//  Copyright © 2018 Joseph Seamons. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
